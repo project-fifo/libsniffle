5f73a7220d2fa199fe27113df917c482da7eb366
Heinz N. Gies <heinz@licenser.net>
2012-05-06 20:39:44 +0200

    Added added_delete machine.

--- src/libsniffle.erl
+++ src/libsniffle.erl
@@ -11,6 +11,7 @@
 %% API
 -export([list_machines/1,
 	 get_machine/2,
+	 delete_machine/2,
 	 list_keys/1,
 	 ping/0,
 	 create_key/4]).
@@ -32,7 +33,10 @@ get_machine(Auth, UUID) ->
     Sniffle = sniffle(),
     gen_server:call(Sniffle, {machines, get, Auth, UUID}).
 
-
+delete_machine(Auth, UUID) ->
+    Sniffle = sniffle(),
+    gen_server:call(Sniffle, {machines, delete, Auth, UUID}).
+    
 list_keys(Auth) ->
     Sniffle = sniffle(),
     gen_server:call(Sniffle, {keys, list, Auth}).
